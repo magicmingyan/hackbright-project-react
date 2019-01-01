f411e5d8e663c93142265d522451eb3b74146e1e
Ming Yan <mingyan.vt@gmail.com>
2019-01-01 15:43:10 -0800

    updated seed

--- backend/seed.py
+++ backend/seed.py
@@ -4,6 +4,40 @@ from model import Article, connect_to_db, db
 from server import app
 
 #---------------------------------------------------------------------#
+def get_NYT_articles():
+
+    parameters = {"api-key": os.environ["nytimes_api"], 
+                    "section": "World",
+                    "time-period": "7"
+                    }
+
+    article_request_string = "https://api.nytimes.com/svc/mostpopular/v2/mostviewed/{}/{}.json".format(parameters["section"],parameters["time-period"])
+    geo_request_string = "https://api.nytimes.com/svc/semantic/v2/geocodes/query.json"
+
+    article_response = requests.get(article_request_string, params=parameters)
+    article_response_dict = article_response.json()
+    articles = article_response_dict['results']
+
+    filtered_articles = [article for article in articles if len(article["geo_facet"])>0]
+
+    for article in filtered_articles:
+        geo_facet = article["geo_facet"][0]
+        geo_facet = geo_facet.split('(')[0].title()  
+
+        # print(geo_facet)
+        geo_parameters = {"api-key": os.environ["nytimes_api"], 
+                    "name": geo_facet
+                    }
+
+        geo_response = requests.get(geo_request_string, params=geo_parameters)
+        geo_response_dict = geo_response.json()
+
+        if  len(geo_response_dict['results'])>0:
+            article["latitude"] = geo_response_dict['results'][0]['latitude']
+            article["longitude"] = geo_response_dict['results'][0]['longitude']
+
+    return filtered_articles
+
 
 def get_articles():
     """Load artiles from nytimes api into database."""
